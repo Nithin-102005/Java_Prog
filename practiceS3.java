d3f701fa1b0742ec0b7c5443f2514723bd2e8abc
Nithin Srivera R <nithin500210@gmail.com>
2026-07-23 11:16:20 +0530

    nope

---
+++ practiceS3.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+class node{
+    int data;
+    int size;
+    int Top;
+    int[] stack;
+    void Node(int size){
+        this.size = size;
+        this.Top = -1;
+        
+    }
+}
+
+
+
+
+public class practiceS3 {
+    
+}
