427f162c9837dbe991e72fbdf57056bff2feba62
Nithin Srivera R <nithin500210@gmail.com>
2026-07-28 09:42:58 +0530

    nope

---
+++ practiceP1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+import java.util.Arrays;
+public class practiceP1 {
+     static int count = 0 ;
+     static int Fib(int n){
+        count++;
+        if(n<=1){
+            return n;
+        }
+        return Fib(n-1)+Fib(n-2);
+     }
+     public static void main(String[] args) {
+        System.out.println(Fib(10));
+        System.out.println(count);
+     }
+}
