e19eabab7323b04af959e81c701352892cd7e88a
Nithin Srivera R <nithin500210@gmail.com>
2026-08-02 12:43:51 +0530

    nope

---
+++ rec4.java
@@ -0,0 +1,10 @@
+public class rec4 {
+    static int N = 0 ;
+    static char[][] board  = new char[N][N];
+    static void Intialize(){
+        for(int i = 0;i<N;i++){
+            for(int j =0;j<N;j++){
+            }
+        }
+    } 
+}
---
+++ stars.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class stars {
+    public static void main(String[] args) {
+         int n = 5 ;
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=n;j++){
+            if(j==i || j == n-i + 1 ){
+                System.out.println("*");
+            }else{
+                System.out.println(" ");
+            }
+        }
+        System.out.println();
+    }
+    }
+}
